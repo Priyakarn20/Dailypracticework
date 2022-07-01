dc77311ad1797e123033c217300c08050578d70a
Priya Karn <106825459+Priyakarn20@users.noreply.github.com>
2022-07-01 19:21:55 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,72 +1,42 @@
-import React, { useState } from "react";
+import { Form, Button , Badge , Card, Modal , } from 'react-bootstrap';
+import './App.css';
+import Abcd from './a.js';
+import Efg from './b.js';
+import Hij from './c.js';
+import OffCanvasExample from './sb.js';
+import Fgh from './modal.js';
 
 function App() {
-  const [display, setDisplay] = useState("");
-  const [expression, setExpression] = useState([]);
-const[reset, setReset] = useState()
-
-  const handleClick = value => {
-    setDisplay(value);
-    setExpression([...expression, value]);
-  };
-
-  const handleResult = () => {
-    const result = expression
-      .join("")
-      .split(/(\D)/g)
-      .map(value => (value.match(/\d/g) ? parseInt(value, 0) : value))
-      .reduce((acc, value, index, array) => {
-        switch (value) {
-          case "+":
-            return (acc = acc + array[index + 1]);
-          case "-":
-            return (acc = acc - array[index + 1]);
-          case "x":
-            return (acc = acc * array[index + 1]);
-          case "÷":
-            return (acc = acc / array[index + 1]);
-          default:
-            return acc;
-        }
-      });
-    setDisplay(result);
-    setExpression("");
-  };
-
   return (
     <div className="App">
-      <h3 className="display">{display}</h3>
 
-      <span className="expression">{expression}</span>
+      <Abcd />
+      <Efg />
+      <Hij />
+      <OffCanvasExample />
+      <Fgh />
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-      <section className="panel">
-        <section className="numbers">
-          <button onClick={() => handleClick(7)}>7</button>
-          <button onClick={() => handleClick(8)}>8</button>
-          <button onClick={() => handleClick(9)}>9</button>
-
-          <button onClick={() => handleClick(4)}>4</button>
-          <button onClick={() => handleClick(5)}>5</button>
-          <button onClick={() => handleClick(6)}>6</button>
-
-          <button onClick={() => handleClick(1)}>1</button>
-          <button onClick={() => handleClick(2)}>2</button>
-          <button onClick={() => handleClick(3)}>3</button>
-
-          <button onClick={() => handleClick(0)}>0</button>
-        </section>
-
-        <section className="operators">
-        <button onClick={() => handleClick(reset)}>c</button>
-          <button onClick={() => handleClick("÷")}>÷</button>
-          <button onClick={() => handleClick("x")}>x</button>
-          <button onClick={() => handleClick("-")}>-</button>
-          <button onClick={() => handleClick("+")}>+</button>
-          <button onClick={() => handleResult()}>=</button>
-        </section>
-      </section>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
 
-export default App
+export default App;
